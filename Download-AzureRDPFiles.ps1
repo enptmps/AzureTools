b1008370419f7e6587f8ec2a7df35f967cbdd2c7
Pheo <ryan.coates@gmail.com>
2016-09-15 00:16:56 -0600

    Updated description

--- Download-AzureRDPFiles.ps1
+++ Download-AzureRDPFiles.ps1
@@ -2,11 +2,17 @@
 .SYNOPSIS
     Export VM .RDP files for a given Azure resource group. 
 .DESCRIPTION
-    The following script will export all VM .RDP connections to a provided path.  The folder must be created by hand before the script is run. 
-.PARAMETER ServiceName
-    Cloud Service the Azure VM is running under. 
-.PARAMETER SubscriptionName 
-    Name of the Azure subscription being selected. 
+    Author: Ryan Coates
+    Version: 1.0
+
+    Original by Courtenay Bernier @ https://gallery.technet.microsoft.com/scriptcenter/Export-Azure-VM-RDP-files-c4b501ea
+
+    Script will export all VM .RDP connections to a provided path.  The folder must be created by hand before the script is run.
+
+.PARAMETER Path
+    Path to save the RDP files to, must exist already
+.PARAMETER rgName
+    Resource Group containing the VMs.   
 #> 
  
 Param ( 
