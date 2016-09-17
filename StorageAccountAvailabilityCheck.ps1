a35bc26944b5dbed185fe12ad8b6d075022a2291
Pheo <ryan.coates@gmail.com>
2016-09-17 11:34:01 -0600

    added error checking

--- StorageAccountAvailabilityCheck.ps1
+++ StorageAccountAvailabilityCheck.ps1
@@ -3,12 +3,13 @@
 param (
     [Parameter()] [String] $saName
 )
-$StorageAccountNameExists = Get-AzureRmStorageAccountNameAvailability -Name $saName
-$StNameExistsInTheTenant  = (Get-AzureRmStorageAccount | ?{$_.StorageAccountName -eq "$saName"})
-if ((!($StorageAccountNameExists.NameAvailable)) -and ($StNameExistsInTheTenant)){
+[bool]$StorageAccountNameAvailable = (Get-AzureRmStorageAccountNameAvailability -Name $saName).NameAvailable
+[bool]$StNameExistsInTheTenant  = (Get-AzureRmStorageAccount | ?{$_.StorageAccountName -eq "$saName"})
+    Write-Verbose "Storage Name is Owned by Us: $StNameExistsInTheTenant"
+    Write-Verbose "Storage Name is Available: $StorageAccountNameAvailable"
+if ((!($StorageAccountNameAvailable)) -and (!($StNameExistsInTheTenant))){
     Write-Output "Storage account" $saName "name already taken"
     Write-Output "Please choose another Storage Account Name"
-    Write-Output "This Powershell command can be used: Get-AzureRmStorageAccountNameAvailability -Name"
     Break
 }
 else {
