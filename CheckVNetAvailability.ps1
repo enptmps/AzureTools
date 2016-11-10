c3ac610582692517da0b8e2ee0f431fc24b1bb12
Ryan Coates <ryan.coates@gmail.com>
2016-11-09 22:37:05 -0800

    vnet check

---
+++ CheckVNetAvailability.ps1
@@ -0,0 +1,29 @@
+﻿function CheckVNetAvailability
+{
+    Param(
+        [Parameter()]
+        [ValidateScript({If ($_ -match '^([a-zA-Z0-9-]){2,64}$')
+        {
+            $True
+
+        } Else {
+
+            Throw "$_ is not a valid Azure VirtualNetwork Name (2-64 Alphanumeric and hyphen)"
+        
+        }})]
+        $vNetName
+    )
+                    
+    [PSObject]$Hold = Get-AzureRmVirtualNetwork | ?{$_.name -eq "$vNetName"}
+                    
+    If ((!$Hold))
+    {
+        Return $True
+    }
+
+    Else
+    {
+        Return $False
+    }
+
+}
